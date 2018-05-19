bd97f0bbbdbf7426a57df576d4e68ca3aed4be58
Franco <kokosika3@gmail.com>
2018-05-19 11:39:02 -0400

    commit 19-05-2018

--- src/main/java/com/serviexpress/config/HibernateConfig.java
+++ src/main/java/com/serviexpress/config/HibernateConfig.java
@@ -14,6 +14,7 @@ import org.springframework.orm.jpa.vendor.HibernateJpaSessionFactoryBean;
  * <p>
  * 1.0 Franco Cortez - Version inicial.
  */
+@SuppressWarnings("ALL")
 @Configuration
 public class HibernateConfig {
 
--- src/main/java/com/serviexpress/servicios/validators/ValidacionUtil.java
+++ src/main/java/com/serviexpress/servicios/validators/ValidacionUtil.java
@@ -1,43 +1,45 @@
 package com.serviexpress.servicios.validators;
 
+import java.util.Objects;
+
 public class ValidacionUtil {
 
-    public static final Validacion<String> notNullString = ValidacionGenerica.from(s -> s != null);
+    public static final Validacion<String> notNullString = ValidacionGenerica.from(Objects::nonNull);
     public static final Validacion<String> notEmptyString = ValidacionGenerica.from(s -> !s.isEmpty());
-    public static final Validacion<Integer> notNullInteger = ValidacionGenerica.from(s -> s != null);
+    public static final Validacion<Integer> notNullInteger = ValidacionGenerica.from(Objects::nonNull);
     public static final Validacion<Integer> greaterThanZero = ValidacionGenerica.from(s -> s > 0);
 
-    public static final Validacion<String> stringMoreThan(int size) {
+    public static Validacion<String> stringMoreThan(int size) {
         return ValidacionGenerica.from(s -> ((String) s).length() > size);
     }
 
     ;
 
-    public static final Validacion<String> stringLessThan(int size) {
+    public static Validacion<String> stringLessThan(int size) {
         return ValidacionGenerica.from(s -> ((String) s).length() < size);
     }
 
     ;
 
-    public static final Validacion<String> stringBetween(int morethan, int lessThan) {
+    public static Validacion<String> stringBetween(int morethan, int lessThan) {
         return stringMoreThan(morethan).and(stringLessThan(lessThan));
     }
 
     ;
 
-    public static final Validacion<Integer> integerMoreThan(int limit) {
+    public static Validacion<Integer> integerMoreThan(int limit) {
         return ValidacionGenerica.from(s -> s > limit);
     }
 
     ;
 
-    public static final Validacion<Integer> integerLessThan(int size) {
+    public static Validacion<Integer> integerLessThan(int size) {
         return ValidacionGenerica.from(s -> s < size);
     }
 
     ;
 
-    public static final Validacion<Integer> integerBetween(int morethan, int lessThan) {
+    public static Validacion<Integer> integerBetween(int morethan, int lessThan) {
         return integerMoreThan(morethan).and(integerLessThan(lessThan));
     }
 
